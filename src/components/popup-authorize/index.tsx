499896bfb7dd940de40c4008d6c982970a929c99
ahsanahmed321 <ahsan.ahmedffs2@gmail.com>
2022-02-19 14:30:13 +0500

    feat: transactions through Dapp

--- src/components/popup-authorize/index.tsx
+++ src/components/popup-authorize/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { approveAuthRequest } from '../../messaging';
 
 const PopupAuth: React.FunctionComponent<any> = ({ requests }) => {
-    console.log('requests ==>>', requests);
+    console.log('auth requests ==>>', requests);
     return (
         <>
             <p>Popup Auth</p>
--- src/components/popup-sign/index.tsx
+++ src/components/popup-sign/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TypeRegistry } from '@polkadot/types';
+import { approveSignPassword } from '../../messaging';
+
+const registry = new TypeRegistry();
+console.log('registry in sign popup', registry);
 
 const PopupSign: React.FunctionComponent<any> = ({ requests }) => {
-    console.log('requests ==>>', requests);
+    const [password, setPassword] = useState('');
+    const passwordChangeHandler = (e: any): void => {
+        setPassword(e.target.value);
+    };
+
+    // useEffect((): void => {
+    //     if (requests[0]) {
+    //         const { payload } = requests[0].request;
+    //         registry.setSignedExtensions(payload.signedExtensions);
+    //     }
+    // }, [requests]);
+    console.log('sign requests ==>>', requests);
     return (
         <>
             <p>Popup Sign</p>
-            <button type="button">Sign</button>
+            <input
+                type="text"
+                onChange={passwordChangeHandler}
+                value={password}
+            />
+            <button
+                type="button"
+                onClick={() =>
+                    approveSignPassword(requests[0].id, false, password)
+                }
+            >
+                Sign
+            </button>
         </>
     );
 };
