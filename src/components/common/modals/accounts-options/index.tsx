55e85c591fb6995ab711411e94447c45f378c485
AbdulSamad <samad13354@gmail.com>
2022-03-09 15:10:15 +0500

    feat: redirecting toshow seed screen added in the accounts-modal section

--- src/components/common/modals/accounts-options/index.tsx
+++ src/components/common/modals/accounts-options/index.tsx
@@ -17,6 +17,16 @@ import {
     TitleDiv,
 } from './styles';
 import { fonts, images } from '../../../../utils';
+
+import {
+    setAccountCreationStep,
+    setTempSeed,
+} from '../../../../redux/slices/activeAccount';
+import {
+    ACCOUNT_DELETION_WARNING,
+    WARNING,
+} from '../../../../utils/app-content';
+
 import { MyAccountsProps } from './types';
 import { RootState } from '../../../../redux/store';
 import accounts from '../../../../utils/accounts';
@@ -25,7 +35,7 @@ import AuthModal from '../authorization';
 
 const { crossIcon, visibilityOn } = images;
 const { mainHeadingfontFamilyClass } = fonts;
-const { deleteAccount, renameAccount } = accounts;
+const { deleteAccount, renameAccount, GenerateSeedPhrase } = accounts;
 
 const MyAccounts: React.FunctionComponent<MyAccountsProps> = (props) => {
     const { open, handleClose, style, onSelection } = props;
@@ -70,22 +80,6 @@ const MyAccounts: React.FunctionComponent<MyAccountsProps> = (props) => {
     //     );
     // };
 
-    const arr = [
-        { name: 'View Accounts', image: visibilityOn, navigation: '/accounts' },
-        {
-            name: 'Add Account',
-            image: visibilityOn,
-        },
-        { name: 'New Derive Account', navigation: '' },
-        { name: 'Import Account', navigation: '/import-wallet' },
-        {
-            name: 'Export Account',
-            modal: (e: boolean) => setAuthScreenModal(e),
-        },
-        { name: 'Rename Account', modal: () => setAuthScreenModal(true) },
-        { name: 'Remove Account', modal: (e: boolean) => warnModalHandler(e) },
-    ];
-
     const warningModal = {
         open: openWarnModal,
         handleClose: () => setOpenWarnModal(false),
@@ -101,9 +95,36 @@ const MyAccounts: React.FunctionComponent<MyAccountsProps> = (props) => {
             px: 2,
             pb: 3,
         },
-        mainText: 'WARNING',
-        subText: 'ACCOUNT_DELETION_WARNING',
+        mainText: WARNING,
+        subText: ACCOUNT_DELETION_WARNING,
     };
+
+    const accountCreation = (): void => {
+        const newSeed = GenerateSeedPhrase();
+        // setSeedToPass(newSeed);
+        dispatch(setAccountCreationStep(1));
+        dispatch(setTempSeed(newSeed));
+        navigate('/show-seed', {
+            state: { newSeed },
+        });
+    };
+
+    const arr = [
+        { name: 'View Accounts', image: visibilityOn, navigation: '/accounts' },
+        {
+            name: 'Add Account',
+            image: visibilityOn,
+            modal: () => accountCreation(),
+        },
+        { name: 'New Derive Account', navigation: '' },
+        { name: 'Import Account', navigation: '/import-wallet' },
+        {
+            name: 'Export Account',
+            modal: (e: boolean) => setAuthScreenModal(e),
+        },
+        { name: 'Rename Account', modal: () => setAuthScreenModal(true) },
+        { name: 'Remove Account', modal: (e: boolean) => warnModalHandler(e) },
+    ];
 
     return (
         <Modal open={open} onClose={handleClose} className="Dark-bg-network">
@@ -162,25 +183,16 @@ const MyAccounts: React.FunctionComponent<MyAccountsProps> = (props) => {
                             >
                                 <HorizontalContentDiv>
                                     <PlainIcon />
-                                    {/* <OptionText
-                                        className={mainHeadingfontFamilyClass}
-                                    >
-                                        {account.name}
-                                    </OptionText> */}
-                                    {/* <img src={account.image} 
-                                    alt="icon" /> */}
                                     <OptionText
                                         className={mainHeadingfontFamilyClass}
                                         onClick={() => {
                                             console.log('click', account);
-                                            // eslint-disable-next-line no-nested-ternary
                                             account.navigation
                                                 ? navigate(account.navigation)
                                                 : dispatch(account.modal(true));
                                         }}
                                     >
-                                        {' '}
-                                        {account.name}{' '}
+                                        {account.name}
                                     </OptionText>
                                 </HorizontalContentDiv>
                             </OptionRow>
