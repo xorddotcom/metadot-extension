9380de2d328cbbec074496a465012d05e0e88e99
ahsanahmed321 <ahsan.ahmedffs2@gmail.com>
2022-03-09 16:51:28 +0500

    fix: tabview issue fixed

--- src/redux/slices/accounts.ts
+++ src/redux/slices/accounts.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { AccountJson } from 'metadot-extension-base/background/types';
-import { Account, Accounts } from '../types';
+import { Accounts } from '../types';
 
 const initialState: Accounts = {};
 
@@ -8,22 +8,23 @@ export const accountsSlice = createSlice({
     name: 'accounts',
     initialState,
     reducers: {
-        addAccount: (state, action: PayloadAction<Account>) => {
-            return {
-                ...state,
-                [action.payload.publicKey]: action.payload,
-            };
-        },
+        // addAccount: (state, action: PayloadAction<Account>) => {
+        //     return {
+        //         ...state,
+        //         [action.payload.publicKey]: action.payload,
+        //     };
+        // },
 
-        deleteAccount: (state, action: PayloadAction<string>) => {
-            const copyState = { ...state };
-            delete copyState[action.payload as keyof Accounts];
-            return copyState;
-        },
+        // deleteAccount: (state, action: PayloadAction<string>) => {
+        //     const copyState = { ...state };
+        //     delete copyState[action.payload as keyof Accounts];
+        //     return copyState;
+        // },
 
         updateAccounts: (state, action: PayloadAction<AccountJson[]>) => {
             console.log('action payload', action.payload);
             const newState: any = {};
+
             action.payload.forEach((account: any) => {
                 newState[account.address] = {
                     publicKey: account.address,
@@ -36,10 +37,13 @@ export const accountsSlice = createSlice({
             console.log('new state', newState);
             return newState;
         },
+
+        resetAccountsSlice: (state) => {
+            return {};
+        },
     },
 });
 
-export const { addAccount, deleteAccount, updateAccounts } =
-    accountsSlice.actions;
+export const { updateAccounts, resetAccountsSlice } = accountsSlice.actions;
 
 export default accountsSlice.reducer;
