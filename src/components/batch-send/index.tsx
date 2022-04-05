8e2fac1926e8e314ff7f2c3e92b6f72735e8bcbe
ahsanahmed321 <ahsan.ahmedffs2@gmail.com>
2022-04-05 14:13:33 +0500

    change: batch transaction script added with authmodal

--- src/components/batch-send/index.tsx
+++ src/components/batch-send/index.tsx
@@ -1,26 +1,53 @@
-import React, { KeyboardEvent } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import type { ApiPromise as ApiPromiseType } from '@polkadot/api';
+import { EventRecord } from '@polkadot/types/interfaces';
 import { Header } from '../common';
 import { Wrapper, HorizontalContentDiv } from '../common/wrapper';
 import BatchCreateView from './create-view';
 import BatchConfirmView from './confirm-view';
 import { SubHeading } from '../common/text';
-import { SEND } from '../../constants';
+import { SEND, DASHBOARD } from '../../constants';
+import useDispatcher from '../../hooks/useDispatcher';
 
-import { images } from '../../utils';
+import { images, accounts } from '../../utils';
 
 import { Recepient } from './types';
+import { RootState } from '../../redux/store';
+
+import { AuthModal } from '../common/modals';
+import {
+    setAuthScreenModal,
+    setIsResponseModalOpen,
+    setConfirmSendModal,
+} from '../../redux/slices/modalHandling';
 
 const { ToggleOn } = images;
+const { signTransaction, isPasswordSaved } = accounts;
 
 const BatchSend: React.FunctionComponent = () => {
     const navigate = useNavigate();
+    const generalDispatcher = useDispatcher();
+    const currReduxState = useSelector((state: RootState) => state);
+    const api = currReduxState.api.api as unknown as ApiPromiseType;
+    const { authScreenModal } = currReduxState.modalHandling;
+    const { publicKey } = currReduxState.activeAccount;
 
     const [step, setStep] = React.useState(0);
     const [recepientList, setRecepientList] = React.useState<Recepient[]>([
         { amount: '0.001', address: 'abc' },
         { amount: '0.002', address: 'xyz' },
     ]);
+    const [savePassword, setSavePassword] = React.useState(false);
+    const [passwordSaved, setPasswordSaved] = React.useState(false);
+
+    useEffect(() => {
+        isPasswordSaved(publicKey).then((res) => {
+            setPasswordSaved(!res);
+            setSavePassword(!res);
+        });
+    }, []);
 
     const handleSendSwitch = (): void => {
         navigate(SEND);
@@ -46,6 +73,94 @@ const BatchSend: React.FunctionComponent = () => {
         const newState = [...recepientList];
         newState[index].amount = value;
         setRecepientList([...recepientList]);
+    };
+
+    const openAuthModal = (): void => {
+        generalDispatcher(() => setAuthScreenModal(true));
+    };
+
+    const signTransactionHandler = async (
+        tx: any,
+        address: string,
+        password: string
+    ): Promise<any> => {
+        const nonce = await api?.rpc?.system?.accountNextIndex(address);
+        const signer = api?.createType('SignerPayload', {
+            method: tx,
+            nonce,
+            genesisHash: api?.genesisHash,
+            blockHash: api?.genesisHash,
+            runtimeVersion: api?.runtimeVersion,
+            version: api?.extrinsicVersion,
+        });
+        const txPayload: any = api?.createType(
+            'ExtrinsicPayload',
+            signer.toPayload(),
+            { version: api?.extrinsicVersion }
+        );
+        const txHex = txPayload.toU8a(true);
+        let signature;
+        try {
+            signature = await signTransaction(
+                address,
+                password,
+                txHex,
+                'substrate',
+                false
+            );
+        } catch (err) {
+            throw new Error('Invalid Password!');
+        }
+        tx.addSignature(address, signature, txPayload);
+        return tx;
+    };
+
+    const sendTransaction = async (
+        address: string,
+        password: string
+    ): Promise<boolean> => {
+        console.log('sending transaction ==>>', recepientList);
+        const txs = recepientList.map((recepient) => {
+            return api.tx.balances.transfer(
+                recepient.address,
+                BigInt(
+                    Number(recepient.amount) *
+                        10 ** api.registry.chainDecimals[0]
+                )
+            );
+        });
+
+        const batchTx = api.tx.utility.batch(txs);
+
+        const signedTx = await signTransactionHandler(
+            batchTx,
+            address,
+            password
+        );
+
+        await signedTx
+            .send(({ status, events }: any) => {
+                const txResSuccess = events.filter(({ event }: EventRecord) =>
+                    api?.events?.system?.ExtrinsicSuccess.is(event)
+                );
+                const txResFail = events.filter(({ event }: EventRecord) =>
+                    api?.events?.system?.ExtrinsicFailed.is(event)
+                );
+                if (status.isInBlock) {
+                    if (txResFail.length >= 1) {
+                        navigate(DASHBOARD);
+                    }
+                    if (txResSuccess.length >= 1) {
+                        navigate(DASHBOARD);
+                    }
+                }
+            })
+            .catch(() => {
+                navigate(DASHBOARD);
+                return false;
+            });
+
+        return true;
     };
 
     return (
@@ -81,8 +196,33 @@ const BatchSend: React.FunctionComponent = () => {
                     addressChangeHandler={addressChangeHandler}
                     amountChangeHandler={amountChangeHandler}
                     deleteRecepient={deleteRecepient}
+                    sendTransaction={openAuthModal}
                 />
             )}
+
+            <AuthModal
+                publicKey={publicKey}
+                open={authScreenModal}
+                handleClose={() => {
+                    generalDispatcher(() => setAuthScreenModal(false));
+                    generalDispatcher(() => setConfirmSendModal(true));
+                }}
+                onConfirm={sendTransaction}
+                functionType={
+                    passwordSaved ? 'PasswordSaved' : 'PasswordNotSaved'
+                }
+                savePassword={savePassword}
+                setSavePassword={setSavePassword}
+                style={{
+                    width: '290px',
+                    background: '#141414',
+                    position: 'relative',
+                    bottom: 30,
+                    p: 2,
+                    px: 2,
+                    pb: 3,
+                }}
+            />
         </Wrapper>
     );
 };
