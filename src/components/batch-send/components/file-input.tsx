d3a6a78951d60fd8928a1daa7ba24333144c6f43
ahsanahmed321 <ahsan.ahmedffs2@gmail.com>
2022-04-06 11:52:06 +0500

    change: csv file validation added

--- src/components/batch-send/components/file-input.tsx
+++ src/components/batch-send/components/file-input.tsx
@@ -1,43 +1,86 @@
-import React, { useRef, useEffect } from 'react';
-import { SubHeading, MainText } from '../../common/text';
+import React, { useEffect } from 'react';
+import { decodeAddress, encodeAddress } from '@polkadot/keyring';
+import { hexToU8a, isHex } from '@polkadot/util';
+import { SubHeading, MainText, WarningText } from '../../common/text';
 import { fonts, images } from '../../../utils';
 import { FileInputDiv } from '../style';
 
 import { FileInputProps, Recepient } from '../types';
 
 const { CSVIcon } = images;
-const { mainHeadingfontFamilyClass } = fonts;
+const { subHeadingfontFamilyClass, mainHeadingfontFamilyClass } = fonts;
 
 const FileInput: React.FunctionComponent<FileInputProps> = ({
     addRecepient,
 }) => {
     const [csvFile, setCsvFile] = React.useState<any>();
+    const [csvData, setCsvData] = React.useState<any>();
+    const [error, setError] = React.useState<string>('');
     const hiddenFileInput = React.useRef<HTMLInputElement>(null);
+
+    const isValidAddressPolkadotAddress = (address: string): boolean => {
+        try {
+            encodeAddress(
+                isHex(address) ? hexToU8a(address) : decodeAddress(address)
+            );
+            return true;
+        } catch (err) {
+            return false;
+        }
+    };
 
     useEffect(() => {
         if (csvFile) {
-            const file = csvFile;
-            const reader = new FileReader();
+            try {
+                const file = csvFile;
+                const reader = new FileReader();
 
-            reader.onload = function (e) {
-                const text = e.target?.result as string;
-                const keys = text?.slice(0, text.indexOf('\n')).split(',');
-                const values = text?.slice(text.indexOf('\n') + 1).split('\n');
+                reader.onload = function (e) {
+                    const text = e.target?.result as string;
+                    const keys = text?.slice(0, text.indexOf('\n')).split(',');
 
-                const recepientList = values.map((value) => {
-                    return {
-                        [keys[0]]: value.split(',')[0],
-                        [keys[1]]: value.split(',')[1],
-                    };
-                });
+                    const values = text
+                        ?.slice(text.indexOf('\n') + 1)
+                        .split('\n');
 
-                recepientList.pop();
-                addRecepient(recepientList as unknown as Recepient[]);
-            };
+                    const recepientList = values.map((value) => {
+                        return {
+                            [keys[0]]: value.split(',')[0],
+                            [keys[1]]: value.split(',')[1],
+                        };
+                    });
+                    recepientList.pop();
+                    setCsvData(recepientList);
+                };
 
-            reader.readAsText(file);
+                reader.readAsText(file);
+            } catch (e: any) {
+                console.log(e);
+            }
         }
     }, [csvFile]);
+
+    useEffect(() => {
+        if (csvData) {
+            const allowedKeys = ['address', 'amount'];
+
+            const keysValidation = allowedKeys.every((val) =>
+                Object.keys(csvData[0]).includes(val)
+            );
+            const valuesValidation = csvData.every(
+                (val: Recepient) =>
+                    isValidAddressPolkadotAddress(val.address) &&
+                    !Number.isNaN(Number(val.amount))
+            );
+
+            if (keysValidation && valuesValidation) {
+                addRecepient(csvData);
+            } else {
+                setError('Invalid csv');
+            }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [csvData]);
 
     const handleClick = (operation: string): void => {
         try {
@@ -74,6 +117,16 @@ const FileInput: React.FunctionComponent<FileInputProps> = ({
                     {csvFile ? csvFile.name : 'Choose File'}
                 </SubHeading>
             </FileInputDiv>
+            {error && (
+                <WarningText
+                    id="warning-text-3"
+                    mb="10px"
+                    className={subHeadingfontFamilyClass}
+                    visibility
+                >
+                    Invalid csv file!
+                </WarningText>
+            )}
             <input
                 id="csv-file"
                 type="file"
